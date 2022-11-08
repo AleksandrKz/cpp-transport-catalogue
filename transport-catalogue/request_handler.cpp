689d1fb324228cc42ed3cbe5481937070d5776d4
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-11-08 20:36:15 +0300

    sprint12 final project

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,6 +1,6 @@
 #include "request_handler.h"
 
-RequestHandler::RequestHandler(const TransportCatalogue& tc, renderer::RendererMap& renderer) : tc_(tc), renderer_(renderer) {
+RequestHandler::RequestHandler(const TransportCatalogue& tc, const TransportRouter& tr, renderer::RendererMap& renderer) : tc_(tc), tr_(tr), renderer_(renderer) {
 }
 
 
@@ -18,10 +18,12 @@ void RequestHandler::PrintStat() {
         space = true;
         if (req.type == "Bus")
             ParseRequestBus(req);
-        else if(req.type == "Stop")
+        else if (req.type == "Stop")
             ParseRequestStop(req);
         else if (req.type == "Map")
             ParseRequestMap(req);
+        else if (req.type == "Route")
+            ParseRequestRouting(req);
     }
     std::cout << "\n]";
 }
@@ -112,4 +114,33 @@ void RequestHandler::ParseRequestStop(const Request& rq) const {
 void RequestHandler::ParseRequestMap(const Request& rq) const {
     json::Document doc = renderer_.GetMap(tc_.GetBuses(), rq.request_id);
     json::Print(doc, std::cout);
+}
+
+void RequestHandler::ParseRequestRouting(const Request& rq) const {
+    using namespace std;
+    if (const Stop* stop_from = tc_.SearchStop(rq.route.from)) {
+        if (const Stop* stop_to = tc_.SearchStop(rq.route.to)) {
+            if (auto route_info = tr_.GetRouteInfo(stop_from, stop_to)) {
+                auto [wieght, edges] = route_info.value();
+
+                json::Print(
+                    json::Document{
+                    json::Builder{}.StartDict()
+                    .Key("items"s).Value(tr_.GetEdgesItems(edges))
+                    .Key("total_time"s).Value(wieght)
+                    .Key("request_id"s).Value(rq.request_id).EndDict().Build()
+                    }, std::cout
+                    );
+                return;
+            }
+        }
+    } 
+    json::Print(
+        json::Document{
+            json::Builder{}.StartDict()
+            .Key("error_message"s).Value("not found"s)
+            .Key("request_id"s).Value(rq.request_id)
+            .EndDict().Build()
+        }, std::cout
+    );
 }
