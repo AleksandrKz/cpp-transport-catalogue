f8d0e9c6284db0adc7a6fb6fffaf65cb5c73993a
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-11-08 20:36:19 +0300

    sprint12 final project

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -3,18 +3,26 @@
 #include <iostream>
 #include <sstream>
 #include "transport_catalogue.h"
+#include "transport_router.h"
 #include "map_renderer.h"
 #include "json_builder.h"
+#include "json.h"
+
+struct RouteFromTo {
+    std::string from;
+    std::string to;
+};
 
 struct Request {
     std::string type;
     std::string name;
     int request_id;
+    RouteFromTo route;
 };
 
 class RequestHandler {
 public:
-    RequestHandler(const TransportCatalogue& tc, renderer::RendererMap& renderer);
+    RequestHandler(const TransportCatalogue& tc, const TransportRouter& tr, renderer::RendererMap& renderer);
     void AddToRequest(Request&& rq);
     void PrintStat();
 
@@ -22,9 +30,11 @@ private:
     void ParseRequestBus(const Request& rq) const;
     void ParseRequestStop(const Request& rq) const;
     void ParseRequestMap(const Request& rq) const;
+    void ParseRequestRouting(const Request& rq) const;
 
     
     const TransportCatalogue& tc_;
+    const TransportRouter& tr_;
     renderer::RendererMap& renderer_;
     std::vector<Request> request_;
 };
