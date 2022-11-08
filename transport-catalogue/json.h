8b1ce37c81649b889be3e2ac2bdbccb2d75ee9da
Aleksandr Kuzyk <ya.a-leksandr@yandex.ru>
2022-11-08 20:35:23 +0300

    sprint12 final project

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -8,82 +8,80 @@
 
 namespace json {
 
-class Node;
-using Dict = std::map<std::string, Node>;
-using Array = std::vector<Node>;
+    class Node;
+    using Dict = std::map<std::string, Node>;
+    using Array = std::vector<Node>;
 
-class ParsingError : public std::runtime_error {
-public:
-    using runtime_error::runtime_error;
-};
+    class ParsingError : public std::runtime_error {
+    public:
+        using runtime_error::runtime_error;
+    };
 
-class Node final
-    : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
-public:
-    using variant::variant;
-    using Value = variant;
+    class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+    public:
+        using variant::variant;
+        using Value = variant;
 
-    bool IsInt() const;
+        bool IsInt() const;
 
-    int AsInt() const;
+        int AsInt() const;
 
-    bool IsPureDouble() const;
+        bool IsPureDouble() const;
 
-    bool IsDouble() const;
+        bool IsDouble() const;
 
-    double AsDouble() const;
+        double AsDouble() const;
 
-    bool IsBool() const;
+        bool IsBool() const;
 
-    bool AsBool() const;
+        bool AsBool() const;
 
-    bool IsNull() const;
+        bool IsNull() const;
 
-    bool IsArray() const;
+        bool IsArray() const;
 
-    const Array& AsArray() const;
+        const Array& AsArray() const;
 
-    bool IsString() const;
+        bool IsString() const;
 
-    const std::string& AsString() const;
+        const std::string& AsString() const;
 
-    bool IsDict() const;
+        bool IsDict() const;
 
-    const Dict& AsDict() const;
+        const Dict& AsDict() const;
 
-    bool operator==(const Node& rhs) const;
+        bool operator==(const Node& rhs) const;
 
-    const Value& GetValue() const;
-};
+        const Value& GetValue() const;
+    };
 
-inline bool operator!=(const Node& lhs, const Node& rhs) {
-    return !(lhs == rhs);
-}
+    inline bool operator!=(const Node& lhs, const Node& rhs) {
+        return !(lhs == rhs);
+    }
+ 
+    class Document {
+    public:
+        explicit Document(Node root) : root_(std::move(root)) {
+        }
 
-class Document {
-public:
-    explicit Document(Node root)
-        : root_(std::move(root)) {
+        const Node& GetRoot() const {
+            return root_;
+        }
+
+    private:
+        Node root_;
+    };
+
+    inline bool operator==(const Document& lhs, const Document& rhs) {
+        return lhs.GetRoot() == rhs.GetRoot();
     }
 
-    const Node& GetRoot() const {
-        return root_;
+    inline bool operator!=(const Document& lhs, const Document& rhs) {
+        return !(lhs == rhs);
     }
 
-private:
-    Node root_;
-};
+    Document Load(std::istream& input);
 
-inline bool operator==(const Document& lhs, const Document& rhs) {
-    return lhs.GetRoot() == rhs.GetRoot();
-}
-
-inline bool operator!=(const Document& lhs, const Document& rhs) {
-    return !(lhs == rhs);
-}
-
-Document Load(std::istream& input);
-
-void Print(const Document& doc, std::ostream& output);
+    void Print(const Document& doc, std::ostream& output);
 
 }  // namespace json
